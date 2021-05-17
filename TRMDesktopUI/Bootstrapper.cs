371a68cf42a7173343b23705b054e4aa24141d3c
Jason247Miller <jason.miller66@yahoo.com>
2021-05-17 14:54:37 -0400

    Register DB Tables
    The tables for the basic register system.

--- TRMDesktopUI/Bootstrapper.cs
+++ TRMDesktopUI/Bootstrapper.cs
@@ -1,30 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using Caliburn.Micro;
 using TRMDesktopUI.ViewModels;
-using System.Collections; 
+
 
 namespace TRMDesktopUI
 {
     public class Bootstrapper : BootstrapperBase
     {
+        //handles the instantiation of most of the classes, new key word will seldomly be used 
         private SimpleContainer _container = new SimpleContainer();
         public Bootstrapper()
-        {
-            Initialize();
+        {   
+            Initialize(); //initialize the bootstrapper processes 
         }
         protected override void Configure()
         {
-            _container.Instance(_container);
 
+            //sets the instance equal itself 
+            _container.Instance(_container);
+            //singletons
             _container
                 .Singleton<IWindowManager, WindowManager>()
                 .Singleton<IEventAggregator, EventAggregator>();
-
+            //reflection 
             GetType().Assembly.GetTypes()
                 .Where(type => type.IsClass)
                 .Where(type => type.Name.EndsWith("ViewModel"))
@@ -32,17 +33,18 @@ namespace TRMDesktopUI
                 .ForEach(viewModelType => _container.RegisterPerRequest(
                     viewModelType, viewModelType.ToString(), viewModelType));
         }
+
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
-            //on start launch shell view model as the base view. 
+            //instead of launching main.xaml launch our code in the SHellViewModel
             DisplayRootViewFor<ShellViewModel>(); 
         }
         protected override object GetInstance(Type service, string key)
-        {
+        {   //gets the instance of the type and name via a container 
             return _container.GetInstance(service, key);
         }
         protected override IEnumerable<object> GetAllInstances(Type service)
-        {
+        {  
             return _container.GetAllInstances(service);
         }
         protected override void BuildUp(object instance)
